0e62b13e95c0372d91ab4dd2c1bd92df277ca07e
ClosegamerHOME <closegamer@mail.ru>
2019-10-27 14:06:18 +0300

    Schedule FRONT

---
+++ client/src/containers/Home/components/DayTabShow.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { MDBTableEditable, MDBBtn,MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import axios from 'axios';
+
+
+
+class DayTabShow extends Component {
+  table = React.createRef();
+  rows = null;
+
+  state = {
+    isLoading: false,
+    isSaving: false,
+    error: ''
+  };
+
+  componentDidMount() {
+    const day = this.props.day;
+
+    return axios
+      .post(`/api/days/load`, { day })
+      .then(response => {
+        if (response.data.success) {
+          this.rows = response.data.rows || [['', '', '', '', '']];
+          const allRows = this.rows;
+          this.setState({
+            isLoading: false,
+            isSaving: false,
+            error: ''
+          });
+
+            let time = null;
+            let hitZoneMaster = null;
+            let hitZoneLesson = null;
+            let tatamiZoneMaster = null;
+            let tatamiZoneLesson = null;
+
+          allRows.forEach(row => {
+              time = row[0];
+              hitZoneMaster = row[1];
+              hitZoneLesson = row[2];
+              tatamiZoneMaster = row[3];
+              tatamiZoneLesson = row[4];
+          });
+          console.log('time: ', time, 'hitZoneMaster: ',hitZoneMaster, 'hitZoneLesson: ', hitZoneLesson, 'tatamiZoneMaster: ', tatamiZoneMaster, 'tatamiZoneLesson: ', tatamiZoneLesson);
+        } else {
+          this.setState({ isLoading: false, error: response.data.error });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          isLoading: false,
+          error: error.response.data.error
+        });
+      });
+  }
+
+  
+  render() {
+    const { isLoading, error } = this.state;
+    
+    if (isLoading) return null;
+    
+    const columns = [
+        {
+            label: 'Время',
+            field: 'time',
+            sort: 'asc'
+        },
+        {
+            label: 'Ударная зона - Тренер',
+            field: 'hzm',
+            sort: 'asc'
+        },
+        {
+            label: 'Ударная зона - Занятие',
+            field: 'hzl',
+            sort: 'asc'
+        },
+        {
+            label: 'Зона татами - Тренер',
+            field: 'tzm',
+            sort: 'asc'
+        },
+        {
+            label: 'Зона татами - Занятие',
+            field: 'tzl',
+            sort: 'asc'
+        }
+      ];
+
+    console.log('this.rows: ', this.rows);
+    console.log('columns: ',columns);
+
+    return (
+      <>
+        <MDBTable responsive>
+            <MDBTableHead columns={columns}/>
+            <MDBTableBody rows={this.rows} />
+        </MDBTable> 
+       
+      </>
+    );
+  }
+}
+
+DayTabShow.defaultProps = {
+  day: 'monday'
+};
+
+export default DayTabShow;
---
+++ client/src/containers/Home/components/Schedule.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink, MDBIcon } from
+"mdbreact";
+import DayTabShow from './DayTabShow.jsx';
+
+class TabsJustified extends Component {
+state = {
+  activeItemJustified: "1"
+}
+
+toggleJustified = tab => e => {
+  if (this.state.activeItemJustified !== tab) {
+    this.setState({
+      activeItemJustified: tab
+    });
+  }
+};
+
+render() {
+    return (
+      <MDBContainer>
+        <MDBNav tabs className="nav-justified" color='indigo'>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "1"} onClick={this.toggleJustified("1")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Понедельник
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "2"} onClick={this.toggleJustified("2")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Вторник
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "3"} onClick={this.toggleJustified("3")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Среда
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "4"} onClick={this.toggleJustified("4")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Четверг
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "5"} onClick={this.toggleJustified("5")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Пятница
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "6"} onClick={this.toggleJustified("6")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Суббота
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#" active={this.state.activeItemJustified === "7"} onClick={this.toggleJustified("7")} role="tab" >
+              <MDBIcon icon="clipboard-list" /> Воскресенье
+            </MDBNavLink>
+          </MDBNavItem>
+        </MDBNav>
+        <MDBTabContent
+          className="card"
+          activeItem={this.state.activeItemJustified}
+        >
+          <MDBTabPane tabId="1" role="tabpanel">
+            <DayTabShow day={'monday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="2" role="tabpanel">
+            <DayTabShow day={'tuesday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="3" role="tabpanel">
+            <DayTabShow day={'wednesday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="4" role="tabpanel">
+            <DayTabShow day={'thursday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="5" role="tabpanel">
+            <DayTabShow day={'friday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="6" role="tabpanel">
+            <DayTabShow day={'saturday'} />
+          </MDBTabPane>
+          <MDBTabPane tabId="7" role="tabpanel">
+            <DayTabShow day={'sunday'} />
+          </MDBTabPane>
+        </MDBTabContent>
+      </MDBContainer>
+    );
+  }
+}
+
+export default TabsJustified;
