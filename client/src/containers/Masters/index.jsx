628ec6a1e338401687642d06925221aa48623257
ClosegamerHOME <closegamer@mail.ru>
2019-10-28 01:09:59 +0300

    Adaptiveness enchanced + 2 Masters hardcoded

--- client/src/containers/Masters/index.jsx
+++ client/src/containers/Masters/index.jsx
@@ -4,6 +4,8 @@ import master1 from '../../img/masters/Rybalko/index.jpeg';
 import master2 from '../../img/masters/Kazantsev/index.jpeg';
 import master3 from '../../img/masters/Litoshik/index.jpeg';
 import master4 from '../../img/masters/Eslemesov/index.jpeg';
+import master5 from '../../img/masters/Kodin/index.jpeg';
+import master6 from '../../img/masters/Egorov/index.jpeg';
 import './styles.css';
 
 function MastersPage(props) {
@@ -18,29 +20,43 @@ function MastersPage(props) {
             <MDBRow>
               <MDBCol>
                 <MDBRow>
-                  <MDBCol size={3} className='text-center'>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center'>
+                    <a href='/masters/Master5'>
+                      <img src={master5} className='img-fluid mastersBoard' />
+                    </a>
+                    <h2>Виталий Кодин</h2>
+                  </MDBCol>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center'>
                     <a href='/masters/Master1'>
-                      <img src={master1} className='mastersBoard' />
+                      <img src={master1} className='img-fluid mastersBoard' />
                     </a>
                     <h2>Валентин Рыбалко</h2>
                   </MDBCol>
-                  <MDBCol size={3} className='text-center gap'>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center gap'>
                     <a href='/masters/Master2'>
-                      <img src={master2} className='mastersBoard' />
+                      <img src={master2} className='img-fluid mastersBoard' />
                     </a>
                     <h2>Станислав Казанцев</h2>
                   </MDBCol>
-                  <MDBCol size={3} className='text-center'>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center'>
                     <a href='/masters/Master3'>
-                      <img src={master3} className='mastersBoard' />
+                      <img src={master3} className='img-fluid mastersBoard' />
                     </a>
                     <h2>Игорь Литошик</h2>
                   </MDBCol>
-                  <MDBCol size={3} className='text-center'>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center'>
                     <a href='/masters/Master4'>
-                      <img src={master4} className='mastersBoard' />
+                      <img src={master4} className='img-fluid mastersBoard' />
                     </a>
                     <h2>Арслан Еслемесов</h2>
+                  </MDBCol>
+                  <MDBCol xs='12' sm='4' md='4' className='text-center'>
+                    <a href='/masters/Master6'>
+                      <img src={master6} className='img-fluid mastersBoard' />
+                    </a>
+                    <h2>Владимир Егоров</h2>
                   </MDBCol>
                 </MDBRow>
               </MDBCol>
