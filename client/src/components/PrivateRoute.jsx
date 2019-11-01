26bd269e36efa20f8645453f82f867868817953c
agrigoriev <agrigoriev@avinfors.ru>
2019-11-01 16:52:01 +0300

    Private root changed to user.role == admin

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  console.log(user);
-  // console.log(user);
-  // console.log(isLoggedIn);
+  console.log(user.role);
   return (
     <Route
       {...rest}
       render={props =>
-        user !== 'SuperAdmin' ? <Redirect to='/' /> : <Component {...props} />
+        user.role !== 'admin' ? <Redirect to='/' /> : <Component {...props} />
       }
     />
   );
