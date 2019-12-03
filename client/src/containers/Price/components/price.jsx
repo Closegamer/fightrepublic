5f78d5617a5e0eb00f692f11718c6d990b277cb3
agrigoriev <agrigoriev@avinfors.ru>
2019-12-03 20:26:14 +0300

    Price page changed

--- client/src/containers/Price/components/price.jsx
+++ client/src/containers/Price/components/price.jsx
@@ -1,212 +1,55 @@
-import React from 'react';
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBTable,
-  MDBTableBody,
-  MDBTableHead
-} from 'mdbreact';
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import "../styles.css";
 
 function PriceInfo(props) {
   return (
     <React.Fragment>
-      <MDBContainer className='main-container' fluid>
+      <MDBContainer className="main-container" fluid>
         <MDBRow>
-          <MDBCol xl='12' xs='12' className='contentArea-container '>
-          <h2 className="h1-responsive font-weight-bold text-center my-5">
-            Цены
-          </h2>
+          <MDBCol xl="12" xs="12" className="contentArea-container ">
+            <h2 className="h1-responsive font-weight-bold text-center my-5">
+              Цены
+            </h2>
             <MDBRow>
-              <MDBCol className='text-center'>
-                <MDBTable striped responsive>
-                  <MDBTableHead>
-                    <tr className='text-center'>
-                      <th>Вид карты</th>
-                      <th>Год</th>
-                      <th>6 месяцев</th>
-                      <th>3 месяца</th>
-                      <th>1 месяц</th>
-                    </tr>
-                  </MDBTableHead>
-                  <MDBTableBody>
-                    <tr className='text-center'>
-                      <td>
-                        Безлимитный абонемент
-                        <br />
-                        (6 раз в неделю)
-                      </td>
-                      <td>
-                        72000
-                        <br />
-                        244 тренировки
-                      </td>
-                      <td>
-                        43500
-                        <br />
-                        144 тренировки
-                      </td>
-                      <td>
-                        25500
-                        <br />
-                        72 тренировки
-                      </td>
-                      <td>
-                        9600
-                        <br />
-                        24 тренировки
-                      </td>
-                    </tr>
-                    <tr className='text-center'>
-                      <td>
-                        Абонемент на 1 вид единоборств
-                        <br />
-                        (4 раза в неделю)
-                      </td>
-                      <td>
-                        57600
-                        <br />
-                        192 тренировки
-                        <br />
-                        (1 тр.-300р)
-                      </td>
-                      <td>
-                        33600
-                        <br />
-                        96 тренировок
-                        <br />
-                        (1 тр.-350р)
-                      </td>
-                      <td>
-                        19200
-                        <br />
-                        48 тренировок
-                        <br />
-                        (1 тр.-400р)
-                      </td>
-                      <td>
-                        7200
-                        <br />
-                        16 тренировок
-                        <br />
-                        (1 тр.-450р)
-                      </td>
-                    </tr>
-                    <tr className='text-center'>
-                      <td>
-                        Абонемент на 1 вид единоборств
-                        <br />
-                        (3 раза в неделю)
-                      </td>
-                      <td>
-                        43200
-                        <br />
-                        144 тренировки
-                        <br />
-                        (1тр.-300р)
-                      </td>
-                      <td>
-                        25200
-                        <br />
-                        72 тренировки
-                        <br />
-                        (1 тр.-350р)
-                      </td>
-                      <td>
-                        14400
-                        <br />
-                        36 тренировок
-                        <br />
-                        (1 тр.-400р)
-                      </td>
-                      <td>
-                        6000
-                        <br />
-                        12 тренировок
-                        <br />
-                        (1 тр.-500р)
-                      </td>
-                    </tr>
-                    <tr className='text-center'>
-                      <td>
-                        Абонемент на 1 вид единоборств
-                        <br />
-                        (2 раза в неделю)
-                      </td>
-                      <td>
-                        35000
-                        <br />
-                        100 тренировок
-                        <br />
-                        (1 тр.-350р)
-                      </td>
-                      <td>
-                        20000
-                        <br />
-                        50 тренировок
-                        <br />
-                        (1 тр.-400р)
-                      </td>
-                      <td>
-                        11250
-                        <br />
-                        25 тренировок
-                        <br />
-                        (1 тр.-450р)
-                      </td>
-                      <td>
-                        4800
-                        <br />
-                        8 тренировок
-                        <br />
-                        (1 тр.-600р)
-                      </td>
-                    </tr>
-                    <tr className='text-center'>
-                      <td>
-                        Абонемент на 1 вид единоборств
-                        <br />
-                        (1 раз в неделю)
-                        <br />
-                      </td>
-                      <td>
-                        19200
-                        <br />
-                        48 тренировок
-                        <br />
-                        (1 тр.-400р)
-                      </td>
-                      <td>
-                        10800
-                        <br />
-                        24 тренировки
-                        <br />
-                        (1 тр.-550р)
-                      </td>
-                      <td>
-                        6600
-                        <br />
-                        12 тренировок
-                        <br />
-                        (1 тр.-550р)
-                      </td>
-                      <td>
-                        2500
-                        <br />
-                        4 тренировки
-                        <br />
-                        (1 тр.-625р)
-                      </td>
-                    </tr>
-                    <tr className='text-center'>
-                      <td>Разовое посещение</td>
-                      <td colSpan='4'>1000</td>
-                    </tr>
-                  </MDBTableBody>
-                </MDBTable>
+              <MDBCol className="text-left">
+                <ul className="noMarker">
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" />{" "}
+                    Безлимитные клубные карты от 1 - 12 месяцев.
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" />{" "}
+                    Безлимитные дневные карты с 7:00 - 17:00 на 6,12 месяцев.
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" /> Клубная
+                    карта "Студенческая".
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" /> Детские
+                    клубные карты на 1 месяц (4,8,12 занятий).
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" /> Гостевой
+                    визит.
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" /> Гостевой
+                    визит детский.
+                  </li>
+                  <li>
+                    <MDBIcon icon="certificate" className="redStar" /> Блок из
+                    персональных тренировок.
+                  </li>
+                </ul>
+                <br />
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol className="text-left">
                 <p>
-                  Групповые занятия: Бокс, Тайский бокс, ММА, Кроссфит +
-                  посещение сауны, парковка, бесплатное полотенце
+                  ЦЕНЫ УТОЧНЯЙТЕ У МЕНЕДЖЕРОВ ПО ТЕЛЕФОНУ: +7 (495) 220-26-24
                 </p>
               </MDBCol>
             </MDBRow>
@@ -220,4 +63,3 @@ function PriceInfo(props) {
 PriceInfo.propTypes = {};
 
 export default PriceInfo;
-
